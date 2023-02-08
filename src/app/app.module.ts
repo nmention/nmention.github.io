57ed14d009bbe81b286ee68774b7645b641edded
Rollet Gabrielle <s2200823@ic2s112-12.univ-lemans.fr>
2023-02-08 11:03:07 +0100

    squelette searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    SearchbarComponent,
 
   ],
   imports: [
