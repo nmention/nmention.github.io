48f7ceb20de65de58d3b9f6c751f076483b5403f
Rollet Gabrielle <s2200823@ic2s130-11.univ-lemans.fr>
2023-03-09 14:11:14 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,33 @@
-import {Component, OnInit} from '@angular/core'
-import {ApiService} from './api.service';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from './services/post.service';
-
-import * as $ from "jquery";
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Movie_track';
-  posts:any;
-  constructor(private service:PostService) {}
-  ngOnInit() {
-
-      this.service.getPosts()
-
-        .subscribe(response => {
-
-          this.posts = response;
-
-        });
-  }
+// Ici on crée et déclare 3 variables pour les relier aux enfants
+export class AppComponent implements OnInit {
+// resultat API de movie data base
+item: any;
+// token d'accès pour l'api movie data base
+accessToken: string;
+constructor(private service:PostService, private route: ActivatedRoute) {
+  this.accessToken = ''
+}
+ngOnInit() {
+this.route.queryParams.subscribe(params => {
+if (params['code'] !== '') {
+this.accessToken = params['code']
+}
+})
+}
+// Voici la fonction qui récupère toutes les infos des api enappelant le service
+getDataFromAPI(formQuery: any) {
+if (!(formQuery === '') && this.accessToken !== '') {
+// ici le subcribe va servir à suivre le changement de reponse donnée par le service
+this.service.getPosts(formQuery).subscribe(response => {this.item = response;
+});
+}
+}
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -12,17 +12,14 @@ import { HttpClient } from '@angular/common/http';
 
 export class PostService {
 
-  private url = 'http://jsonplaceholder.typicode.com/posts';
+  private url = 'https://api.themoviedb.org/3/search/movie?api_key=627ac22760c37360d262266fadac96ed&language=fr-FR&page=1&include_adult=false&query=Shrek';
 
    
 
   constructor(private httpClient: HttpClient) { }
+  getPosts(arg : string){
 
-  
-
-  getPosts(){
-
-    return this.httpClient.get(this.url);
+    return this.httpClient.get(this.url + encodeURIComponent(arg));
 
   }
 
